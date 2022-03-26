952fa40675e81acd3cf65fa5e9672d6ccc18e71d
Bar <ogoun.d@gmail.com>
2022-03-26 14:29:54 +0300

    Debug prints

--- src/actions/searchAPI.js
+++ src/actions/searchAPI.js
@@ -5,6 +5,9 @@ import APIcall from './server';
 const baseURL = process.env.NODE_ENV === 'production' ? 'https://icotube-server.herokuapp.com/' : 'http://localhost:5000';
 
 export const getPostsAPI = (before, after) => {
+    console.log(process.env);
+    console.log(process.env.NODE_ENV);
+    console.log(baseURL);
     APIcall({
         method: "GET",
         url: 'posts',
