db1cd6a51ee27e5cf0a069f12fe98f2ecf976217
Bar <ogoun.d@gmail.com>
2022-03-26 14:11:12 +0300

    Print env
    cant find server url?

--- src/actions/searchAPI.js
+++ src/actions/searchAPI.js
@@ -5,6 +5,8 @@ import APIcall from './server';
 const baseURL = process.env.SERVER_URL || 'http://localhost:5000';
 
 export const getPostsAPI = (before, after) => {
+    console.log('env!');
+    console.log(process.env);
     APIcall({
         method: "GET",
         url: 'posts',
