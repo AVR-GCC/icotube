374baa7e8cade829e2c49c083ef10ce30b810e1a
Bar <ogoun.d@gmail.com>
2022-03-26 14:23:50 +0300

    Choose the url from node env
    Just to finish this

--- src/actions/searchAPI.js
+++ src/actions/searchAPI.js
@@ -2,11 +2,9 @@
 import axios from 'axios';
 import APIcall from './server';
 
-const baseURL = process.env.SERVER_URL || 'http://localhost:5000';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://icotube-server.herokuapp.com/' : 'http://localhost:5000';
 
 export const getPostsAPI = (before, after) => {
-    console.log('env!');
-    console.log(process.env);
     APIcall({
         method: "GET",
         url: 'posts',
