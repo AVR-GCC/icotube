c9a36be0afa6afd99b8f5f8abd12539d3e7b2d35
Bar <ogoun.d@gmail.com>
2022-03-26 14:03:10 +0300

    Use server url environment variable
    For heroku

--- src/actions/searchAPI.js
+++ src/actions/searchAPI.js
@@ -2,55 +2,7 @@
 import axios from 'axios';
 import APIcall from './server';
 
-// export const getHistoryServer = (setSearchHistory) => {
-//     APIcall({
-//         method: "GET",
-//         url: 'history',
-//         events: [
-//             async () => {
-//             },
-//             async res => {
-//                 await setSearchHistory(res.data.slice(1));
-//             }
-//         ]
-//     });
-// }
-
-// export const searchServer = (search, setLoading, doSearch, setList) => {
-//     APIcall({
-//         method: "GET",
-//         url: `search/${search}`,
-//         events: [
-//             async () => {
-//                 await setLoading(true);
-//                 doSearch(search)
-//             },
-//             async res => {
-//                 await setList(res.data);
-//                 await setLoading(false);
-//             }
-//         ]
-//     });
-// }
-
-// export const searchServerPersist = (search, setLoading, doSearch, setList) => {
-//     APIcall({
-//         method: "POST",
-//         url: `search/${search}`,
-//         events: [
-//             async () => {
-//                 await setLoading(true);
-//                 doSearch(search)
-//             },
-//             async res => {
-//                 await setList(res.data);
-//                 await setLoading(false);
-//             }
-//         ]
-//     });
-// }
-
-const baseURL = 'http://localhost:5000';
+const baseURL = process.env.SERVER_URL || 'http://localhost:5000';
 
 export const getPostsAPI = (before, after) => {
     APIcall({
--- src/pages/home.js
+++ src/pages/home.js
@@ -9,7 +9,6 @@ import SelectedPost from '../components/selectedPost';
 import { useParams, useNavigate } from 'react-router-dom';
 
 function Home({ history, location }) {
-    console.log('Home component ready!!!');
     const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);
 
