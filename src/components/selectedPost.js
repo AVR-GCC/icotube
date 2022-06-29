d006f4c191ddc0a3880054f08874ab9a1510bbef
Bar <ogoun.d@gmail.com>
2022-06-29 21:56:36 +0300

    Reorganize selected post
    And replace text X with X icon (close)

--- src/components/selectedPost.js
+++ src/components/selectedPost.js
@@ -1,6 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
 import '../styles/selectedPost.css';
-import { FullscreenRounded, ReplyRounded, ArrowBackIosNewRounded, ArrowForwardIosRounded } from '@mui/icons-material';
+import {
+    FullscreenRounded,
+    ReplyRounded,
+    ArrowBackIosNewRounded,
+    ArrowForwardIosRounded,
+    Close
+} from '@mui/icons-material';
 import ReactPlayer from 'react-player';
 
 function SelectedPost({
@@ -34,54 +40,88 @@ function SelectedPost({
         cursor: 'pointer'
     }
 
+    const _topIcons = () => (
+        <div className='social'>
+            <div className='socialButton'><FullscreenRounded /></div>
+            <div className='socialButton'><ReplyRounded /></div>
+        </div>
+    );
+
+    const _XButton = () => <div className='XButton' onClick={XClick}><Close /></div>;
+
+    const _postTopBar = () => (
+        <div className='postTopBar'>
+            {_topIcons()}
+            {_XButton()}
+        </div>
+    );
+
+    const _leftArrow = () => (
+        <div className='arrowPart'>
+            {!!leftClick && (
+                <ArrowBackIosNewRounded
+                    style={arrowStyle}
+                    onClick={leftClick}
+                />
+            )}
+        </div>
+    );
+
+    const _rightArrow = () => (
+        <div className='arrowPart'>
+            {!!rightClick && (
+                <ArrowForwardIosRounded
+                    style={arrowStyle}
+                    onClick={rightClick}
+                />
+            )}
+        </div>
+    );
+
+    const _player = () => (
+        <div
+            className='playerPart'
+            ref={playerPartRef}
+        >
+            <ReactPlayer
+                url={post.videoUrl}
+                muted={false}
+                controls={true}
+                height={playerSize.height}
+                width={playerSize.width}
+            />
+        </div>
+    );
+
+    const _videoSection = () => (
+        <div className='videoSection'>
+            {_leftArrow()}
+            {_player()}
+            {_rightArrow()}
+        </div>
+    );
+
+    const _infoSection = () => (
+        <div className='infoSection'>
+            <div className='infoBox'>
+                <div className='titleText'>{post.title}</div>
+                <div className='infoText'>{post.description}</div>
+                <div className='linkText'>{post.homepage}</div>
+            </div>
+        </div>
+    );
+
+    const _bottomPart = () => (
+        <div className='bottomPart'>
+            {_videoSection()}
+            {_infoSection()}
+        </div>
+    );
+
     return (
         <div className='selectedPost'>
-            <div className='postTopBar'>
-                <div className='social'>
-                    <div className='socialButton'><FullscreenRounded /></div>
-                    <div className='socialButton'><ReplyRounded /></div>
-                </div>
-                <div className='XButton' onClick={XClick}>X</div>
-            </div>
-            <div className='bottomPart'>
-                <div className='videoSection'>
-                    <div className='arrowPart'>
-                        {!!leftClick && (
-                            <ArrowBackIosNewRounded
-                                style={arrowStyle}
-                                onClick={leftClick}
-                            />
-                        )}
-                    </div>
-                    <div
-                        className='playerPart'
-                        ref={playerPartRef}
-                    >
-                        <ReactPlayer
-                            url={post.videoUrl}
-                            muted={false}
-                            controls={true}
-                            height={playerSize.height}
-                            width={playerSize.width}
-                        />
-                    </div>
-                    <div className='arrowPart'>
-                        {!!rightClick && (
-                            <ArrowForwardIosRounded
-                                style={arrowStyle}
-                                onClick={rightClick}
-                            />
-                        )}
-                    </div>
-                </div>
-                <div className='infoSection'>
-                    <div className='infoBox'>
-                        <div className='titleText'>{post.title}</div>
-                        <div className='infoText'>{post.description}</div>
-                        <div className='linkText'>{post.homepage}</div>
-                    </div>
-                </div>
-            </div>
+            {_postTopBar()}
+            {_bottomPart()}
         </div>
     );
 }
