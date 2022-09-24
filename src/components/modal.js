08590a28f51e58bbd659d3c099e4b4a85848a546
Bar <ogoun.d@gmail.com>
2022-09-24 19:37:17 -0400

    Update padding for modal location calculation

--- src/components/modal.js
+++ src/components/modal.js
@@ -6,8 +6,7 @@ const Modal = ({
     height = 500,
     width = 500
 }) => {
-    // 15 is half of the padding
-    const padding = 30;
+    const padding = 60;
     const top = window.innerHeight / 2 - height / 2 - padding / 2;
     const left = window.innerWidth / 2 - width / 2 - padding / 2;
     return (
