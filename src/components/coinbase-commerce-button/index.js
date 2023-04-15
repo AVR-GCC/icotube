340bbe4cca0f0bd5ec51e29e5d516f89ec34d675
Bar <ogoun.d@gmail.com>
2023-04-15 12:41:41 -0400

    font stuff

--- src/components/coinbase-commerce-button/index.js
+++ src/components/coinbase-commerce-button/index.js
@@ -70,7 +70,6 @@ class CoinbaseCommerceButton extends React.Component<Props, State>{
 
   render(){
     const {showModal} = this.state;
-    console.log('showModal', showModal);
     const {onLoad, onChargeSuccess, onChargeFailure, checkoutId, chargeId, customMetadata, onPaymentDetected, disableCaching, wrapperStyle} = this.props;
     const iFrameProps = {onLoad, onChargeSuccess, onChargeFailure, checkoutId, chargeId, onPaymentDetected, disableCaching};
     const buttonProps = getButtonProps(this.props);
